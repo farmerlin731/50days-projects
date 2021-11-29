fc7936437898fdebbf7d2bc78e19f75ad0ae6a75
farmerlin731 <69831413+farmerlin731@users.noreply.github.com>
2021-11-29 18:21:58 +0800

    Fix: card:hover -> mouse-enter event (@home-page)

--- script.js
+++ script.js
@@ -2,12 +2,24 @@ cards = document.querySelectorAll('.projects-cards');
 //為什麼我下const會錯...
 //Uncaught SyntaxError: Identifier 'cards' has already been declared
 
+// cards.forEach((item)=>{
+//     console.log(item);
+//     item.addEventListener('click',(e)=>{
+//         cleanCardState();
+//         item.classList.add('active');
+//     });
+// });
+
 cards.forEach((item)=>{
     console.log(item);
-    item.addEventListener('click',(e)=>{
-        cleanCardState();
+    item.addEventListener('mouseenter',(e)=>{
         item.classList.add('active');
     });
+
+    item.addEventListener('mouseleave',(e)=>{
+        item.classList.remove('active');
+    });
+    
 });
 
 
