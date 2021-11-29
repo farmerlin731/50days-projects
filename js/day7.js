2c0cc9b7efa1aa5cdb3ded8b1f33f4629c4c6134
farmerlin731 <69831413+farmerlin731@users.noreply.github.com>
2021-11-29 21:12:36 +0800

    Fix: bugs @ day7

--- js/day7.js
+++ js/day7.js
@@ -7,10 +7,19 @@ console.log(`left:${left}`);
 console.log(`r:${right}`);
 console.log(`c:${container}`);
 
-
+// -- for web -- //
 left.addEventListener('mouseenter',(e)=> container.classList.add('left-focus'));
 left.addEventListener('mouseleave',(e)=> container.classList.remove('left-focus'));
-
-
 right.addEventListener('mouseenter',(e)=> container.classList.add('right-focus'));
 right.addEventListener('mouseleave',(e)=> container.classList.remove('right-focus'));
+
+
+// -- for mobile -- //
+left.addEventListener('click',()=>{
+    container.classList.remove('right-focus');
+    container.classList.add('left-focus');
+})
+right.addEventListener('click',()=>{
+    container.classList.remove('left-focus');
+    container.classList.add('right-focus');
+})
