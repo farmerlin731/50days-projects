b1db3d53c9d6ac5031b9adcef7a0ddfb5f32e407
farmerlin731 <69831413+farmerlin731@users.noreply.github.com>
2021-12-14 11:01:05 +0800

    Create the files of html & modify the router.

--- js/day8.js
+++ js/day8.js
@@ -3,6 +3,6 @@ const labels = document.querySelectorAll('.form-control label');
 labels.forEach((item) => {
     console.log(item.textContent);
     item.innerHTML =item.textContent.split('')
-    .map((char,index)=>`<span style="transition-delay: ${index*50}ms">${char}</span>`)
+    .map((char,index)=>`<span style="transition-delay: ${index*70}ms">${char}</span>`)
     .join('')
 })
